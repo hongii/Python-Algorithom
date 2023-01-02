943e4869613b5fc53a41dae572dc970dba197464
khe <khj778878@naver.com>
2023-01-02 16:05:55 +0900

    k번쨰 약수 구하기

---
+++ 코드 구현력 기르기/1. K번째 약수.py
@@ -0,0 +1,16 @@
+import sys
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\코드 구현력 기르기\\input.txt"
+sys.stdin = open(filePath,"rt");
+
+count = 0
+n, k = map(int, input().split())
+
+for i in range(1,n+1):
+	if(n % i == 0):
+		count += 1
+	if(count == k):
+		print(i)
+		break
+else:
+	print(-1)
+
