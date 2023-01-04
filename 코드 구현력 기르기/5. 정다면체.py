eb025e698d20b05a00c2b897a6f01d0384c535f2
khe <khj778878@naver.com>
2023-01-04 18:49:36 +0900

    5. 정다면체

--- 코드 구현력 기르기/5. 정다면체.py
+++ 코드 구현력 기르기/5. 정다면체.py
@@ -3,12 +3,12 @@ import sys
 # sys.stdin = open(filePath,"rt");
 
 n, m = map(int, input().split())
-# sum = [[0]*(n+1) for _ in range(m+1)]
+sum = [[0]*(n+1) for _ in range(m+1)]
 cnt = [0] * (n+m+1)
 
 for i in range(1, m+1):
 	for j in range(1, n+1):
-		# sum[i][j] = i+j
+		sum[i][j] = i+j
 		cnt[i+j] += 1
 
 maxNum = max(cnt)
