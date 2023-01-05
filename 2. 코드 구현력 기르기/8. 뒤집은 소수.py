646755dfbd24b8cda48eb4f6992601eb8921651d
khe <khj778878@naver.com>
2023-01-05 16:31:05 +0900

    8. 뒤집은 소수

--- 2. 코드 구현력 기르기/8. 뒤집은 소수.py
+++ 2. 코드 구현력 기르기/8. 뒤집은 소수.py
@@ -1,18 +1,17 @@
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\2. 코드 구현력 기르기\\input.txt"
-# sys.stdin = open(filePath,"rt");
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\2. 코드 구현력 기르기\\input.txt"
+sys.stdin = open(filePath,"rt");
 
 n = int(input())
 numList = map(str, input().split())
-
 
 def isPrime(x):
 	x = int(x)
 	if (x == 1):
 		return False
-
+		
 	for i in range(2, x):
-		if (x % i == 0):
+		if(x % i == 0):
 			return False
 	else:
 		return True
