59edec08efee975f87ead4d6e288aafeb81c3986
khe <khj778878@naver.com>
2023-01-07 11:01:53 +0900

    [자료구조]: 2. 쇠막대기

---
+++ 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/2. 쇠막대기.py
@@ -0,0 +1,23 @@
+import sys
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\1. 자료구조 활용(스택, 큐, 해쉬, 힙)\\input.txt"
+sys.stdin = open(filePath, "rt")
+inputList = list(input());
+
+stack = []
+last = ""
+totalCount = 0
+
+for x in inputList:
+	if(stack and x == ")"):
+		if(last == "("):
+			stack.pop()
+			totalCount += len(stack)
+		else:
+			stack.pop()
+			totalCount += 1
+	else:
+		stack.append(x)	
+	
+	last = x	
+
+print(totalCount)
--- 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/AA.py
+++ 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/AA.py
@@ -1,28 +1,23 @@
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\2. 코드 구현력 기르기\\input.txt"
+# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\1. 자료구조 활용(스택, 큐, 해쉬, 힙)\\input.txt"
 # sys.stdin = open(filePath, "rt")
+inputList = list(input())
 
-num, k = map(int, input().split())
-num = list(str(num))
 stack = []
-cnt = k
+last = ""
+totalCount = 0
 
-for i in range(len(num)):
-	if (len(stack) == 0):
-		stack.append(num[i])
-
-	elif(cnt > 0):
-		while (stack[-1] < num[i]):
+for x in inputList:
+	if (stack and x == ")"):
+		if (last == "("):
 			stack.pop()
-			cnt -= 1
-			if (len(stack) == 0 or cnt == 0):
-				break
-		stack.append(num[i])
-	
+			totalCount += len(stack)
+		else:
+			stack.pop()
+			totalCount += 1
 	else:
-		stack.append(num[i])
+		stack.append(x)
 
-if (cnt == 0):
-	print("".join(stack))
-else:
-	print("".join(stack[:-cnt]))
+	last = x
+
+print(totalCount)
