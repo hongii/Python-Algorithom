76a62e0a74ea37a089d2bac9d9f7912da5302d4c
khe <khj778878@naver.com>
2023-01-08 20:26:54 +0900

    [자료구조]: 8. 단어 찾기

---
+++ 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/8. 단어 찾기.py
@@ -0,0 +1,34 @@
+import sys
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\1. 자료구조 활용(스택, 큐, 해쉬, 힙)\\input.txt"
+sys.stdin = open(filePath, "rt")
+
+n = int(input())
+words = []
+for i in range(n):
+	words.append(input())
+
+for i in range(n-1):
+	word = input()
+	if word in words:
+		popIdx = words.index(word)
+		words.pop(popIdx)
+print(words[0])
+
+'''
+# solution : dictionary 사용하기
+# 노트에 적어둔 단어(key)의 value 값을 1로 초기화하고, 노트에 쓰여진 단어의 value값을 0으로 수정하고 마지막에 value가 1인 단어 하나를 출력시키면 된다.
+n = int(input())
+words = dict()
+for i in range(n):
+	word = input()
+	words[word] = 1
+
+for i in range(n-1):
+	word = input()
+	words[word] = 0
+
+for key, val in words.items():
+	if(val == 1):	
+		print(key)
+		break
+'''
--- 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/AA.py
+++ 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/AA.py
@@ -1,27 +1,11 @@
-### Queue 활용 ###
-# <dequeue 라이브러리 사용>
-
-# 다시 풀 문제 -> input2 예시에서 wrong answer
-
-from collections import deque
-import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\1. 자료구조 활용(스택, 큐, 해쉬, 힙)\\input.txt"
-# sys.stdin = open(filePath, "rt")
-
-necessary = input()
 n = int(input())
-
+words = []
 for i in range(n):
-	course = input()
-	dq = deque(necessary)
-	for x in course:
-		if x in dq:  # 현수가 계획한 과목이 필수과목에 해당하는지 확인
-			if x != dq.popleft():  # 현수가 계획한 과목이 필수과목의 순서와 다른 경우
-				print("#{0} {1}".format(i+1, "NO"))
-				break
-	else:
-		if len(dq) == 0:  # 필수과목과 현수가 계획한 과목에 모든 필수과목이 제대로 들어있고 그 순서까지 동일한 경우
-			print("#{0} {1}".format(i+1, "YES"))
-		else:  # 필수과목과 현수가 계획한 과목의 순서는 맞긴한데, 필수과목 중 일부가 빠져있는 경우 -> dq에 필수과목이 남아있는 상태
-			print("#{0} {1}".format(i+1, "NO"))
+	words.append(input())
 
+for i in range(n-1):
+	word = input()
+	if word in words:
+		popIdx = words.index(word)
+		words.pop(popIdx)
+print(words[0])
