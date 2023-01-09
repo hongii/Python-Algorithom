98403213889d26b26e9bddc13a70a83d864c1152
khe <khj778878@naver.com>
2023-01-09 17:10:48 +0900

    [자료구조]: 8. 아나그램

--- 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/9. 아나그램.py
+++ 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/9. 아나그램.py
@@ -1,7 +1,7 @@
 # 딕셔너리 해쉬
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\1. 자료구조 활용(스택, 큐, 해쉬, 힙)\\input.txt"
-# sys.stdin = open(filePath, "rt")
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\1. 자료구조 활용(스택, 큐, 해쉬, 힙)\\input.txt"
+sys.stdin = open(filePath, "rt")
 
 str1 = list(input())
 str2 = list(input())
