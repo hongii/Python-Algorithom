2e6d4bb4add3111b79388ab5ee920d42a0e950e8
khe <khj778878@naver.com>
2023-01-10 12:17:38 +0900

    [자료구조]: 10. 최소힙

--- 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/10. 최소힙.py
+++ 1. 자료구조 활용(스택, 큐, 해쉬, 힙)/10. 최소힙.py
@@ -1,7 +1,7 @@
 import heapq as hq
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\1. 자료구조 활용(스택, 큐, 해쉬, 힙)\\input.txt"
-# sys.stdin = open(filePath, "rt")
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\1. 자료구조 활용(스택, 큐, 해쉬, 힙)\\input.txt"
+sys.stdin = open(filePath, "rt")
 
 heap = []
 while True:
