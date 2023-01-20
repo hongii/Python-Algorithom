ad24bef37ebf247b6b4d71d28b0d4b4c94e3c8b2
khe <khj778878@naver.com>
2023-01-20 15:43:48 +0900

    [탐색&시뮬레이션]: 4. 두 리스트 합치기

--- 3. 탐색&시뮬레이션(string,1차원,2차원 리스트탐색)/4. 두 리스트 합치기.py
+++ 3. 탐색&시뮬레이션(string,1차원,2차원 리스트탐색)/4. 두 리스트 합치기.py
@@ -1,6 +1,6 @@
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\3. 탐색&시뮬레이션(string,1차원,2차원 리스트탐색)\\input.txt"
-# sys.stdin = open(filePath, "rt")
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\3. 탐색&시뮬레이션(string,1차원,2차원 리스트탐색)\\input.txt"
+sys.stdin = open(filePath, "rt")
 
 n = int(input())
 nList = list(map(int, input().split()))
@@ -15,7 +15,7 @@ for i in range(n+m):
   elif len(mList) == 0:
     sortedList.append(nList)
     break
-
+  
   nListMin = nList[0]
   mListMin = mList[0]
   if nListMin < mListMin:
@@ -24,10 +24,9 @@ for i in range(n+m):
   else:
     sortedList.append(mListMin)
     mList.pop(0)
-
+    
 for x in sortedList:
   if type(x) is list:
-    # Python 리스트에 Asterisk(*) 를 사용하면 리스트 압축 해제를 할 수 있게 된다. 즉, 리스트 대괄호를 생략하고 출력이 가능하다.
-    print(*x)
+    print(*x)  #Python 리스트에 Asterisk(*) 를 사용하면 리스트 압축 해제를 할 수 있게 된다. 즉, 리스트 대괄호를 생략하고 출력이 가능하다.
   else:
     print(x, end=" ")
