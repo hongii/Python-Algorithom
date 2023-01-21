3c94944094fe5bba43d6353e7572e9ce27546687
khe <khj778878@naver.com>
2023-01-21 15:04:20 +0900

    [탐색&시뮬레이션]: 9. 봉우리

--- 3. 탐색&시뮬레이션(string,1차원,2차원 리스트탐색)/9. 봉우리.py
+++ 3. 탐색&시뮬레이션(string,1차원,2차원 리스트탐색)/9. 봉우리.py
@@ -1,6 +1,6 @@
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\3. 탐색&시뮬레이션(string,1차원,2차원 리스트탐색)\\input.txt"
-# sys.stdin = open(filePath, "rt")
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\3. 탐색&시뮬레이션(string,1차원,2차원 리스트탐색)\\input.txt"
+sys.stdin = open(filePath, "rt")
 
 n = int(input())
 board = [[0]*(n+2) for i in range(n+2)]
