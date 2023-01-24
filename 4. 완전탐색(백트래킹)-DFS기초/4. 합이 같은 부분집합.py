5db4829d96d64706c9019aded09ee6e5e11e0cc8
khe <khj778878@naver.com>
2023-01-24 18:15:16 +0900

    [완전탐색(백트래킹)-DFS기초]: 4. 합이 같은 부분집합

--- 4. 완전탐색(백트래킹)-DFS기초/4. 합이 같은 부분집합.py
+++ 4. 완전탐색(백트래킹)-DFS기초/4. 합이 같은 부분집합.py
@@ -1,13 +1,17 @@
+import sys
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
+sys.stdin = open(filePath, "rt")
 n = int(input())
 numList = list(map(int, input().split()))
 numList.append(-1)
 m = max(numList) + 1
 check = [-1] * m
 
-def dfs(x, idx):
+def dfs(x,idx):
   if x == -1:
     set1 = [i for i in range(1, m) if check[i] == 1]
     set2 = [i for i in range(1, m) if check[i] == 0]
+    # print(set1, set2)
     if sum(set1) == sum(set2):
       return True
     else:
@@ -22,9 +26,9 @@ def dfs(x, idx):
       print("YES")
       exit()
   return False
-
-
+    
 if __name__ == "__main__":
   res = dfs(numList[0], 0)
   if res == False:
     print("NO")
+
