a0b8203ac9672f679ab45d4b5cc4d7e54d81e349
khe <khj778878@naver.com>
2023-01-26 21:39:33 +0900

    [완전탐색(백트래킹)-DFS기초]: 8. 순열 구하기_dfs solution추가

--- 4. 완전탐색(백트래킹)-DFS기초/8. 순열 구하기.py
+++ 4. 완전탐색(백트래킹)-DFS기초/8. 순열 구하기.py
@@ -4,8 +4,31 @@ filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practi
 sys.stdin = open(filePath, "rt")
 n, m = map(int, input().split())
 res = [0] * m
+check = [0] * (n+1)
 cnt = 0
 
+'''solution -> check 배열을 만들어서 중복된 가지부분은 피하기'''
+def dfs(x):
+  global cnt, res
+  if x == m:
+    cnt += 1
+    for i in range(m):
+      print(res[i], end=" ")
+    print()
+  else:
+    for i in range(1, n+1):
+      if check[i] == 0:
+        check[i] = 1
+        res[x] = i
+        dfs(x+1)
+        check[i] = 0 #dfs(x+1)수행 후 돌아와서 다시 check[i]부분을 0으로 되돌려 놔야한다.
+
+
+dfs(0)
+print(cnt)
+
+
+''' dfs 구현해서 풀어본 것
 def dfs(x):
   global cnt, res
   if x == m:
@@ -23,7 +46,7 @@ def dfs(x):
 dfs(0)
 print(cnt)
     
-
+'''
 
 ''' itertools 라이브러리를 이용한 순열 구하기 연습
 n, m = map(int, input().split())
--- 4. 완전탐색(백트래킹)-DFS기초/AA.py
+++ 4. 완전탐색(백트래킹)-DFS기초/AA.py
@@ -1,12 +1,36 @@
-# from itertools import permutations
+from itertools import permutations
 import sys
 # filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
 # sys.stdin = open(filePath, "rt")
 n, m = map(int, input().split())
 res = [0] * m
+check = [0] * (n+1)
 cnt = 0
 
+'''solution -> check 배열을 만들어서 중복된 가지부분은 피하기'''
 
+
+def dfs(x):
+  global cnt, res
+  if x == m:
+    cnt += 1
+    for i in range(m):
+      print(res[i], end=" ")
+    print()
+  else:
+    for i in range(1, n+1):
+      if check[i] == 0:
+        check[i] = 1
+        res[x] = i
+        dfs(x+1)
+        check[i] = 0  # dfs(x+1)수행 후 돌아와서 다시 check[i]부분을 0으로 되돌려 놔야한다.
+
+
+dfs(0)
+print(cnt)
+
+
+''' dfs 구현해서 풀어본 것
 def dfs(x):
   global cnt, res
   if x == m:
@@ -15,17 +39,16 @@ def dfs(x):
     if tmpRes == setRes:
       cnt += 1
       for i in range(m):
-        print(res[i], end=" ")
+        print(res[i], end = " ")
       print()
   else:
     for i in range(1, n+1):
       res[x] = i
       dfs(x+1)
-
-
 dfs(0)
 print(cnt)
-
+    
+'''
 
 ''' itertools 라이브러리를 이용한 순열 구하기 연습
 n, m = map(int, input().split())
