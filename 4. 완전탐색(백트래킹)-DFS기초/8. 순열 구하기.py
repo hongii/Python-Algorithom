7932e3e6705937f3b58e0362c1c11400e1a9f510
khe <khj778878@naver.com>
2023-01-26 20:47:52 +0900

    [완전탐색(백트래킹)-DFS기초]: 8. 순열 구하기_itertools 라이브러리 사용

--- 4. 완전탐색(백트래킹)-DFS기초/8. 순열 구하기.py
+++ 4. 완전탐색(백트래킹)-DFS기초/8. 순열 구하기.py
@@ -1,7 +1,7 @@
 from itertools import permutations 
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
-# sys.stdin = open(filePath, "rt")
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
+sys.stdin = open(filePath, "rt")
 
 n, m = map(int, input().split())
 numList = [i for i in range(1, n + 1)]
