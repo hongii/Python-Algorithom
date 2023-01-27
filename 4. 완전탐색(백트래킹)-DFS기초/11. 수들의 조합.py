3754c9ea8d586e29468b8bc9df53378c279d6f10
khe <khj778878@naver.com>
2023-01-27 17:21:18 +0900

    [완전탐색(백트래킹)-DFS기초]: 11. 수들의 조합

--- 4. 완전탐색(백트래킹)-DFS기초/11. 수들의 조합.py
+++ 4. 완전탐색(백트래킹)-DFS기초/11. 수들의 조합.py
@@ -1,6 +1,6 @@
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
-# sys.stdin = open(filePath, "rt")
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
+sys.stdin = open(filePath, "rt")
 
 n, k = map(int, input().split())
 numList = list(map(int, input().split()))
