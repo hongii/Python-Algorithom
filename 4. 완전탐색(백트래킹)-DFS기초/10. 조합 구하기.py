c77368013106985333c98f127e55721afd3075fd
khe <khj778878@naver.com>
2023-01-27 16:02:42 +0900

    [완전탐색(백트래킹)-DFS기초]: 10. 조합 구하기

--- 4. 완전탐색(백트래킹)-DFS기초/10. 조합 구하기.py
+++ 4. 완전탐색(백트래킹)-DFS기초/10. 조합 구하기.py
@@ -1,7 +1,10 @@
+import sys
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
+sys.stdin = open(filePath, "rt")
+
 n, m = map(int, input().split())
 res = [0] * m
 cnt = 0
-
 
 def dfs(x, startNum):
   global cnt
@@ -14,7 +17,7 @@ def dfs(x, startNum):
     for i in range(startNum, n+1):
       res[x] = i
       dfs(x+1, i+1)
-
+      
 
 dfs(0, 1)
 print(cnt)
