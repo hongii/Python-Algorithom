57513eb5571a115bd6a314481abc2c9c915e1912
khe <khj778878@naver.com>
2023-01-27 18:16:47 +0900

    [완전탐색(백트래킹)-DFS기초]: 11. 수들의 조합_library 사용

--- 4. 완전탐색(백트래킹)-DFS기초/11. 수들의 조합2.py
+++ 4. 완전탐색(백트래킹)-DFS기초/11. 수들의 조합2.py
@@ -1,6 +1,5 @@
 # 상태트리 (가지가 여러개인 트리)
 import sys
-import itertools as it
 filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
 sys.stdin = open(filePath, "rt")
 
@@ -8,10 +7,17 @@ n, k = map(int, input().split())
 numList = list(map(int, input().split()))
 m = int(input())
 cnt = 0
-c = list(it.combinations(numList, k))
 
-for x in c:
-  res = sum(x)
-  if res % m == 0:
-    cnt += 1
+
+def dfs(x, startNum, sum):
+  global cnt
+  if x == k:
+    if sum % m == 0:
+      cnt += 1
+  else:
+    for i in range(startNum, n):
+      dfs(x+1, i+1, sum+numList[i])
+
+
+dfs(0, 0, 0)
 print(cnt)
--- 4. 완전탐색(백트래킹)-DFS기초/AA.py
+++ 4. 완전탐색(백트래킹)-DFS기초/AA.py
@@ -1,20 +1,13 @@
-import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\4. 완전탐색(백트래킹)-DFS기초\\input.txt"
-# sys.stdin = open(filePath, "rt")
+import itertools as it
 
 n, k = map(int, input().split())
 numList = list(map(int, input().split()))
 m = int(input())
 cnt = 0
+c = list(it.combinations(numList, k))
 
-def dfs(x, startNum, sum):
-  global cnt
-  if x == k:
-    if sum % m == 0:
-      cnt += 1
-  else:
-    for i in range(startNum, n):
-      dfs(x+1, i+1 ,sum+numList[i])
-
-dfs(0,0,0)
+for x in c:
+  res = sum(x)
+  if res % m == 0:
+    cnt += 1
 print(cnt)
