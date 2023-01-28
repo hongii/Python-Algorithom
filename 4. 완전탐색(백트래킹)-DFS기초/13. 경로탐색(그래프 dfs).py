54f94ec746cdbc8db94d71d20afc1cc2e6a50098
khe <khj778878@naver.com>
2023-01-28 14:40:09 +0900

    [완전탐색(백트래킹)-DFS기초]: 13. 경로탐색(그래프 dfs)_solution 수정

--- 4. 완전탐색(백트래킹)-DFS기초/13. 경로탐색(그래프 dfs).py
+++ 4. 완전탐색(백트래킹)-DFS기초/13. 경로탐색(그래프 dfs).py
@@ -13,22 +13,22 @@ for i in range(m):
   graph[x][y] = 1
 
 
-def dfs(x, v):  # x는 깊이(depth or level), v는 node 번호
+def dfs(v):  # v는 node번호
 	global path, cnt
 	if v == n:
 		cnt += 1
-		# print(*path)
 	else:
 		for i in range(1, n+1):
 			if graph[v][i] == 1:
 				if check[i] == 0:
 					check[i] = 1
-					dfs(x+1, i)
+					dfs(i)
 					check[i] = 0
 
 check[1] = 1  # 1번노드 부터 n번 노드까지 가는 경로이므로 1번 노드가 시작점이 된다.
-dfs(0, 1)
+dfs(1)
 print(cnt)
+
 
 ''' 첫번째 코드 40점 -> path배열 사용
 import sys
