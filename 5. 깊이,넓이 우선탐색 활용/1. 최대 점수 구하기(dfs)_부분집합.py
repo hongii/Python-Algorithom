7feaf45c35f7fc891ff0b3c094e0f872cee2474e
khe <khj778878@naver.com>
2023-01-30 22:45:09 +0900

    [깊이,넓이 우선탐색 활용]: 1. 최대점수 구하기_부분집합풀이

--- 5. 깊이,넓이 우선탐색 활용/1. 최대 점수 구하기(dfs)_부분집합.py
+++ 5. 깊이,넓이 우선탐색 활용/1. 최대 점수 구하기(dfs)_부분집합.py
@@ -1,13 +1,12 @@
 import sys
-# filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\5. 깊이,넓이 우선탐색 활용\\input.txt"
-# sys.stdin = open(filePath, "rt")
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\5. 깊이,넓이 우선탐색 활용\\input.txt"
+sys.stdin = open(filePath, "rt")
 
 n, m = map(int, input().split())
 maxScoreSum = 0
 score = []
 for i in range(n):
   score.append(list(map(int, input().split())))
-
 
 def dfs(x, scoreSum, time):
   global maxScoreSum
@@ -16,12 +15,12 @@ def dfs(x, scoreSum, time):
   if x == n:
     if maxScoreSum < scoreSum:
       maxScoreSum = scoreSum
-
+    
   else:
       # print(x, scoreSum, time, maxScoreSum)
     dfs(x+1, scoreSum+score[x][0], time+score[x][1])
     dfs(x+1, scoreSum, time)
-
+        
 
 dfs(0, 0, 0)
 print(maxScoreSum)
