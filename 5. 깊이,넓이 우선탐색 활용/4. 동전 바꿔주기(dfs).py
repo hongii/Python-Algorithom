90d356b92135a4f3886f654bf0b0f937e4a549c6
khe <khj778878@naver.com>
2023-01-31 22:37:12 +0900

    [깊이,넓이 우선탐색 활용]: 4. 동전 바꿔주기(dfs)_solution적용

--- 5. 깊이,넓이 우선탐색 활용/4. 동전 바꿔주기(dfs).py
+++ 5. 깊이,넓이 우선탐색 활용/4. 동전 바꿔주기(dfs).py
@@ -1,9 +1,12 @@
 # 다시 풀어볼 문제 -> 아이디어 구상 못함
+import sys
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\5. 깊이,넓이 우선탐색 활용\\input.txt"
+sys.stdin = open(filePath, "rt")
+
 T = int(input())
 k = int(input())
 coins = [list(map(int, input().split())) for _ in range(k)]
 cnt = 0
-
 
 def dfs(x, res):
   global cnt
@@ -15,7 +18,6 @@ def dfs(x, res):
   else:
     for i in range(0, coins[x][1]+1):
       dfs(x+1, res+(coins[x][0]*i))
-
-
+      
 dfs(0, 0)
 print(cnt)
