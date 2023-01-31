790209092ee5908188b7d55bad35369216f995dd
khe <khj778878@naver.com>
2023-01-31 17:43:49 +0900

    [깊이,넓이 우선탐색 활용]: 3. 양팔저울_solution적용

--- 5. 깊이,넓이 우선탐색 활용/3. 양팔저울(dfs).py
+++ 5. 깊이,넓이 우선탐색 활용/3. 양팔저울(dfs).py
@@ -1,9 +1,12 @@
+import sys
+filePath = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\5. 깊이,넓이 우선탐색 활용\\input.txt"
+sys.stdin = open(filePath, "rt")
+
 k = int(input())
 weight = list(map(int, input().split()))
 s = sum(weight)
 check = [0] * (s+1)
 cnt = 0
-
 
 def dfs(x, res):
   if x == k:
@@ -14,9 +17,8 @@ def dfs(x, res):
     dfs(x+1, res-weight[x])
     dfs(x+1, res)
 
-
 dfs(0, 0)
 for i in range(1, s+1):
-  if check[i] == 0:  # 측정 불가능한 물의 무게
+  if check[i] == 0: # 측정 불가능한 물의 무게
     cnt += 1
 print(cnt)
