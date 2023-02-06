407f23bf2a223257ee9c073d9676ba0d978a58cb
khe <khj778878@naver.com>
2023-02-06 15:54:15 +0900

    [깊이,넓이 우선탐색 활용]: 14. 안전영역(bfs)_수정

--- 5. 깊이,넓이 우선탐색 활용/14. 안전영역(bfs).py
+++ 5. 깊이,넓이 우선탐색 활용/14. 안전영역(bfs).py
@@ -10,33 +10,41 @@ res = []
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 dq = deque()
-
+maxHeight = 0
+for i in range(n):
+  tmpMax = max(board[i])
+  if tmpMax > maxHeight:
+    maxHeight = tmpMax
+print(maxHeight)
 for h in range(1, 101):
-	safeZone = [[0]*n for _ in range(n)]
+  if h == maxHeight:
+    break
+  else:
+    safeZone = [[0]*n for _ in range(n)]
 
-	# 높이가 h보다 큰 지역만 check(안전지대)
-	for i in range(n):
-		for j in range(n):
-			if board[i][j] > h:
-				safeZone[i][j] = 1
-	
-	# 안전지대 영역 탐색
-	check = [[0]*n for _ in range(n)]
-	cnt = 0
-	for i in range(n):
-		for j in range(n):
-			if safeZone[i][j] == 1 and check[i][j] == 0:
-				dq.append((i, j))
-				check[i][j] = 1
-				cnt += 1
-				while dq:
-					now = dq.popleft()
-					for k in range(4):
-						x = now[0] + dx[k]
-						y = now[1] + dy[k]
-						if 0 <= x < n and 0 <= y < n  and check[x][y] == 0 and safeZone[x][y] == 1:
-							dq.append((x, y))
-							check[x][y] = 1		
-		res.append(cnt) # 장마철에 잠기는 높이에 따른 안전지대 영역 갯수
-
+    # 높이가 h보다 큰 지역만 check(안전지대)
+    for i in range(n):
+      for j in range(n):
+        if board[i][j] > h:
+          safeZone[i][j] = 1
+    
+    # 안전지대 영역 탐색
+    check = [[0]*n for _ in range(n)]
+    cnt = 0
+    for i in range(n):
+      for j in range(n):
+        if safeZone[i][j] == 1 and check[i][j] == 0:
+          dq.append((i, j))
+          check[i][j] = 1
+          cnt += 1
+          while dq:
+            now = dq.popleft()
+            for k in range(4):
+              x = now[0] + dx[k]
+              y = now[1] + dy[k]
+              if 0 <= x < n and 0 <= y < n  and check[x][y] == 0 and safeZone[x][y] == 1:
+                dq.append((x, y))
+                check[x][y] = 1		
+    res.append(cnt) # 장마철에 잠기는 높이에 따른 안전지대 영역 갯수
+# print(res)
 print(max(res))
