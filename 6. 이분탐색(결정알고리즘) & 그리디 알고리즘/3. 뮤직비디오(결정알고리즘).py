eeb04014415c617fc03ca28f5aff28ebde5993f0
khe <khj778878@naver.com>
2023-02-13 17:43:30 +0900

    [이분탐색(결정알고리즘) & 그리디 알고리즘]: 3. 뮤직비디오(결정알고리즘)_반례추가 및 코드 수정

--- 6. 이분탐색(결정알고리즘) & 그리디 알고리즘/3. 뮤직비디오(결정알고리즘).py
+++ 6. 이분탐색(결정알고리즘) & 그리디 알고리즘/3. 뮤직비디오(결정알고리즘).py
@@ -5,26 +5,24 @@ sys.stdin = open(filePath_desktop, "rt")
 
 n, m = map(int, input().split())
 dvd = list(map(int, input().split()))
-
-maxTime = sum(dvd)
+totalTime = sum(dvd)
+maxTime = max(dvd)
 lt = 1
-rt = maxTime
+rt = totalTime
 res = 0
 while lt <= rt:
   mid = (lt + rt) // 2
   tmpSum = 0
-  cnt = 0
+  cnt = 1
   for x in dvd:
-    tmpSum += x 
-    if tmpSum >= mid:
+    if tmpSum + x > mid:
       cnt += 1
       tmpSum = x
-      # print(mid, cnt, tmpSum)
+    else:
+      tmpSum += x
 
-  if cnt == m:
+  if mid >= maxTime and cnt <= m: # 반례 수정: dvd 최소 용량은 적어도 곡의 길이의 최대값보다는 크거나 같아야 한다. -> mid >= maxTime 조건 추가
     res = mid
-    lt = mid + 1
-  elif cnt < m:
     rt = mid - 1
   else:
     lt = mid + 1
