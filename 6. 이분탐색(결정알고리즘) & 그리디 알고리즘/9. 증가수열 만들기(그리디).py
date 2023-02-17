befb20b3f56b4b94fd25e58dded9e822c0b7a9b5
khe <khj778878@naver.com>
2023-02-17 17:37:29 +0900

    [이분탐색(결정알고리즘) & 그리디 알고리즘]: 9. 증가수열 만들기(그리디)_solution추가

--- 6. 이분탐색(결정알고리즘) & 그리디 알고리즘/9. 증가수열 만들기(그리디).py
+++ 6. 이분탐색(결정알고리즘) & 그리디 알고리즘/9. 증가수열 만들기(그리디).py
@@ -7,6 +7,39 @@ sys.stdin = open(filePath_desktop, "rt")
 n = int(input())
 numList = deque(map(int, input().split()))
 
+# solution 참고
+lt, rt = 0, n-1
+lastNum = 0
+comp = []
+res = ""
+while lt <= rt:
+  # lastNum과 numList의 왼쪽 오른쪽 양쪽값을 각각 비교해서 numList값이 더 크다면 comp 리스트에 추가
+  if lastNum < numList[lt]:
+    comp.append((numList[lt], "L"))
+  if lastNum < numList[rt]:
+    comp.append((numList[rt], "R"))
+
+# 튜플의 1번째 원소값 기준으로 오름차순 정렬
+  comp.sort()
+
+  if len(comp) == 0: # comp 리스트에 아무 원소도 안들어 간 경우
+    break
+  else:
+    res += comp[0][1]
+    lastNum = comp[0][0]
+
+    # lt와 rt가 가르키는 위치 갱신
+    if comp[0][1] == "L":
+      lt += 1
+    else:
+      rt -= 1
+  comp.clear()
+
+print(len(res))
+print(res)
+
+
+'''첫번째 코드 100점
 res = []
 tmp = []
 flagL, flagR = 0, 0
@@ -51,3 +84,4 @@ while numList:
 print(len(res))
 for x in res:
   print(x, end="")
+'''
--- 6. 이분탐색(결정알고리즘) & 그리디 알고리즘/AA.py
+++ 6. 이분탐색(결정알고리즘) & 그리디 알고리즘/AA.py
@@ -1,48 +1,35 @@
 from collections import deque
+
 n = int(input())
 numList = deque(map(int, input().split()))
 
-res = []
-tmp = []
-flagL, flagR = 0, 0
-while numList:
-  if len(numList) == 1:
-    if tmp[-1] < numList[0]:
-      res.append("L")
-      tmp.append(numList[0])
-      numList.pop()
-      break
-    else:
-      break
-  if numList[0] < numList[-1]:
-    if len(tmp) > 0 and tmp[-1] > numList[0]:
-      flagL = 1
-      if tmp[-1] < numList[-1]:
-        res.append("R")
-        tmp.append(numList[-1])
-        numList.pop()
-      else:
-        break
-    else:
-      res.append("L")
-      tmp.append(numList[0])
-      numList.popleft()
-  elif numList[0] > numList[-1]: 
-    if len(tmp) and tmp[-1] > numList[-1]:
-      flagR = 1
-      if tmp[-1] < numList[0]:
-        res.append("L")
-        tmp.append(numList[0])
-        numList.popleft()
-      else:
-        break
-    else:
-      res.append("R")
-      tmp.append(numList[-1])
-      numList.pop()
-  if flagL == flagR == 1:
+# solution 참고
+lt, rt = 0, n-1
+lastNum = 0
+comp = []
+res = ""
+while lt <= rt:
+  # lastNum과 numList의 왼쪽 오른쪽 양쪽값을 각각 비교해서 numList값이 더 크다면 comp 리스트에 추가
+  if lastNum < numList[lt]:
+    comp.append((numList[lt], "L"))
+  if lastNum < numList[rt]:
+    comp.append((numList[rt], "R"))
+
+# 튜플의 1번째 원소값 기준으로 오름차순 정렬
+  comp.sort()
+
+  if len(comp) == 0: # comp 리스트에 아무 원소도 안들어 간 경우
     break
+  else:
+    res += comp[0][1]
+    lastNum = comp[0][0]
+
+    # lt와 rt가 가르키는 위치 갱신
+    if comp[0][1] == "L":
+      lt += 1
+    else:
+      rt -= 1
+  comp.clear()
 
 print(len(res))
-for x in res:
-  print(x, end="")
+print(res)
