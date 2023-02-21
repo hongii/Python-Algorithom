b4eef11d03231bded2b8287c87580820a5bbebf4
khe <khj778878@naver.com>
2023-02-21 20:57:10 +0900

    [동적 계획법]: 7. 가장 높은 탑 쌓기(LIS)

--- 7. 동적 계획법/7. 가장 높은 탑 쌓기(LIS).py
+++ 7. 동적 계획법/7. 가장 높은 탑 쌓기(LIS).py
@@ -1,3 +1,7 @@
+import sys
+filePath_laptop = "C:\\Users\khj77\\OneDrive\\바탕 화면\\Python\\python_Algo_practice\\Python-Algorithom\\7. 동적 계획법\\input.txt"
+filePath_desktop = "C:\\Users\\cywoo\\OneDrive\\바탕 화면\\Python\\python_algo_practice\\Python-Algorithom\\7. 동적 계획법\\input.txt"
+sys.stdin = open(filePath_desktop, "rt")
 n = int(input())
 bricks = [list(map(int, input().split())) for _ in range(n)] #[[밑면넓이, 높이, 무게],[...]...]
 bricks.sort(reverse=True)
